ad1b4d54b2140c7a04b789c42be7fb76a6d405c2
OiNutter <willmckenzie@oinutter.co.uk>
2011-05-10 16:55:41 +0100

    Added initial files
    added load and basic resize functions

---
+++ src/coffee/obscura.coffee
@@ -0,0 +1,37 @@
+#set up global object
+root = exports ? this
+		
+obscura = (img,target) ->
+	
+	#load element
+	@canvas = document.querySelector(target)
+	@context = @canvas.getContext('2d')
+	@image = document.querySelector(img)
+	
+	#load image
+	@load =(x=0,y=0,w=@image.width,h=@image.height)=>
+		@canvas.width = w
+		@canvas.height = h
+		@context.drawImage(@image,x,y,w,h)
+		
+	@resize = (scale,keepProportions=true) =>
+				
+		#check type of scale and convert it to an object if not already
+		if Object.prototype.toString.call(scale) == '[object Array]'
+			scale = 
+				w: scale[0]
+				h:scale[1]
+		else if typeof scale != 'object'
+			scale = 
+				w:scale
+				h:scale
+				
+		#convert percentages to actual values
+		scale.w = if typeof scale.w == 'string' and scale.w.match(/%/) then @image.width * (parseFloat(scale.w)/100) else parseFloat(scale.w) 
+		scale.h = if typeof scale.h == 'string' and scale.h.match(/%/) then @image.height * (parseFloat(scale.h)/100) else parseFloat(scale.h)
+				
+		@context.clearRect(0,0,@canvas.width,@canvas.height)
+		@load(0,0,scale.w,scale.h)
+	
+	return this
+root.obscura = obscura
---
+++ src/obscura.js
@@ -0,0 +1,49 @@
+(function() {
+  var obscura, root;
+  var __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; };
+  root = typeof exports != "undefined" && exports !== null ? exports : this;
+  obscura = function(img, target) {
+    this.canvas = document.querySelector(target);
+    this.context = this.canvas.getContext('2d');
+    this.image = document.querySelector(img);
+    this.load = __bind(function(x, y, w, h) {
+      if (x == null) {
+        x = 0;
+      }
+      if (y == null) {
+        y = 0;
+      }
+      if (w == null) {
+        w = this.image.width;
+      }
+      if (h == null) {
+        h = this.image.height;
+      }
+      this.canvas.width = w;
+      this.canvas.height = h;
+      return this.context.drawImage(this.image, x, y, w, h);
+    }, this);
+    this.resize = __bind(function(scale, keepProportions) {
+      if (keepProportions == null) {
+        keepProportions = true;
+      }
+      if (Object.prototype.toString.call(scale) === '[object Array]') {
+        scale = {
+          w: scale[0],
+          h: scale[1]
+        };
+      } else if (typeof scale !== 'object') {
+        scale = {
+          w: scale,
+          h: scale
+        };
+      }
+      scale.w = typeof scale.w === 'string' && scale.w.match(/%/) ? this.image.width * (parseFloat(scale.w) / 100) : parseFloat(scale.w);
+      scale.h = typeof scale.h === 'string' && scale.h.match(/%/) ? this.image.height * (parseFloat(scale.h) / 100) : parseFloat(scale.h);
+      this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+      return this.load(0, 0, scale.w, scale.h);
+    }, this);
+    return this;
+  };
+  root.obscura = obscura;
+}).call(this);
