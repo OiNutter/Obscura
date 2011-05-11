eda3bd4befb6851c006424a958923e74d35b9ca4
OiNutter <willmckenzie@oinutter.co.uk>
2011-05-11 01:58:44 +0100

    Added Crop, Fit and Rotate functions

--- src/coffee/obscura.coffee
+++ src/coffee/obscura.coffee
@@ -8,15 +8,20 @@ obscura = (img,target) ->
 	@context = @canvas.getContext('2d')
 	@image = document.querySelector(img)
 	
-	#load image
-	@load =(x=0,y=0,w=@image.width,h=@image.height)=>
-		@canvas.width = w
-		@canvas.height = h
+	###
+	load image
+	###
+	@load =(x=0,y=0,w=@image.width,h=@image.height,cw,ch)=>
+		@context.clearRect(0,0,@canvas.width,@canvas.height)
+		@canvas.width = cw ? w
+		@canvas.height = ch ? h
 		@context.drawImage(@image,x,y,w,h)
+		return @
 		
+	###
+	resizes an image
+	###	
 	@resize = (scale,keepProportions=true) =>
-		
-		@load()
 		
 		#check type of scale and convert it to an object if not already
 		if Object.prototype.toString.call(scale) is '[object Array]'
@@ -39,8 +44,84 @@ obscura = (img,target) ->
 			else if scale.h>scale.w or (scale.h==scale.w and @canvas.width > @canvas.height)
 				scale.w = (scale.h/@canvas.height)*@canvas.width
 			
-		@context.clearRect(0,0,@canvas.width,@canvas.height)
 		@load(0,0,scale.w,scale.h)
+		return @
+	
+	###
+	Crops an image
+	###
+	@crop = (x,y,w,h) =>
+		@load(-x,-y,@canvas.width,@canvas.height,w,h)
+		return @
+		
+	###
+	Resizes an image to fit completely into a given space
+	###
+	@fit = (w,h) =>
+		if w>@canvas.width and h>@canvas.height
+			return @
 			
+		if w<h or @canvas.width > @canvas.height or (w is h and @canvas.height > @canvas.width)
+			h = (w/@canvas.width)*@canvas.height
+		else if h<w or @canvas.height>@canvas.width or (h is w and @canvas.width > @canvas.height)
+			w = (h/@canvas.height)*@canvas.width;
+		
+		@load(0,0,w,h)
+		return @
+			
+	#initial load of image
+	@load();
+	
+	###
+	Rotates an image
+	###
+	@rotate = (angle,center='center') =>
+		w = @canvas.width
+		h = @canvas.height
+		if angle is 90 or angle is 120
+			cw = h
+			ch = w
+		else if angle is 180 or angle is 360
+			cw = w
+			ch = h
+		else 	
+			cw = w*Math.cos(angle * Math.PI/180) + h*Math.sin(angle * Math.PI/180)
+			ch = h*Math.cos(angle * Math.PI/180) + w*Math.sin(angle * Math.PI/180)
+			
+		console.log(cw)
+		console.log(ch) 
+		@context.clearRect(0,0,@canvas.width,@canvas.height)
+		@canvas.width = cw
+		@canvas.height = ch
+		
+		if typeof center isnt 'object'
+			if center.match(/(top)/)
+				y = 0
+				y2 = (ch-h)/2
+			else if center.match(/(bottom)/)
+				y = @canvas.height
+				y2 = @canvas.height - (ch-h)/2
+			else if center.match(/(center)/)
+				y = @canvas.height/2
+				y2 = h/2
+				
+			if center.match(/(left)/)
+				x = 0
+				x2 = (cw-w)/2
+			else if center.match(/(middle)/)
+				x = @canvas.width
+				x2 = @canvas.height - (cw-w)/2
+			else if center.match(/(center)/)
+				x = @canvas.width/2
+				x2 = w/2
+		
+		@context.translate(x,y)
+		@context.rotate(angle * Math.PI/180)
+	
+		@context.drawImage(@image,-x2,-y2,w,h)
+		
+		
+		return @
+				
 	return this
 root.obscura = obscura
--- src/obscura.js
+++ src/obscura.js
@@ -1,12 +1,15 @@
 (function() {
   var obscura, root;
   var __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; };
-  root = typeof exports != "undefined" && exports !== null ? exports : this;
+  root = typeof exports !== "undefined" && exports !== null ? exports : this;
   obscura = function(img, target) {
     this.canvas = document.querySelector(target);
     this.context = this.canvas.getContext('2d');
     this.image = document.querySelector(img);
-    this.load = __bind(function(x, y, w, h) {
+    /*
+    	load image
+    	*/
+    this.load = __bind(function(x, y, w, h, cw, ch) {
       if (x == null) {
         x = 0;
       }
@@ -19,15 +22,19 @@
       if (h == null) {
         h = this.image.height;
       }
-      this.canvas.width = w;
-      this.canvas.height = h;
-      return this.context.drawImage(this.image, x, y, w, h);
+      this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+      this.canvas.width = cw != null ? cw : w;
+      this.canvas.height = ch != null ? ch : h;
+      this.context.drawImage(this.image, x, y, w, h);
+      return this;
     }, this);
+    /*
+    	resizes an image
+    	*/
     this.resize = __bind(function(scale, keepProportions) {
       if (keepProportions == null) {
         keepProportions = true;
       }
-      this.load();
       if (Object.prototype.toString.call(scale) === '[object Array]') {
         scale = {
           w: scale[0],
@@ -48,8 +55,83 @@
           scale.w = (scale.h / this.canvas.height) * this.canvas.width;
         }
       }
+      this.load(0, 0, scale.w, scale.h);
+      return this;
+    }, this);
+    /*
+    	Crops an image
+    	*/
+    this.crop = __bind(function(x, y, w, h) {
+      this.load(-x, -y, this.canvas.width, this.canvas.height, w, h);
+      return this;
+    }, this);
+    /*
+    	Resizes an image to fit completely into a given space
+    	*/
+    this.fit = __bind(function(w, h) {
+      if (w > this.canvas.width && h > this.canvas.height) {
+        return this;
+      }
+      if (w < h || this.canvas.width > this.canvas.height || (w === h && this.canvas.height > this.canvas.width)) {
+        h = (w / this.canvas.width) * this.canvas.height;
+      } else if (h < w || this.canvas.height > this.canvas.width || (h === w && this.canvas.width > this.canvas.height)) {
+        w = (h / this.canvas.height) * this.canvas.width;
+      }
+      this.load(0, 0, w, h);
+      return this;
+    }, this);
+    this.load();
+    /*
+    	Rotates an image
+    	*/
+    this.rotate = __bind(function(angle, center) {
+      var ch, cw, h, w, x, x2, y, y2;
+      if (center == null) {
+        center = 'center';
+      }
+      w = this.canvas.width;
+      h = this.canvas.height;
+      if (angle === 90 || angle === 120) {
+        cw = h;
+        ch = w;
+      } else if (angle === 180 || angle === 360) {
+        cw = w;
+        ch = h;
+      } else {
+        cw = w * Math.cos(angle * Math.PI / 180) + h * Math.sin(angle * Math.PI / 180);
+        ch = h * Math.cos(angle * Math.PI / 180) + w * Math.sin(angle * Math.PI / 180);
+      }
+      console.log(cw);
+      console.log(ch);
       this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
-      return this.load(0, 0, scale.w, scale.h);
+      this.canvas.width = cw;
+      this.canvas.height = ch;
+      if (typeof center !== 'object') {
+        if (center.match(/(top)/)) {
+          y = 0;
+          y2 = (ch - h) / 2;
+        } else if (center.match(/(bottom)/)) {
+          y = this.canvas.height;
+          y2 = this.canvas.height - (ch - h) / 2;
+        } else if (center.match(/(center)/)) {
+          y = this.canvas.height / 2;
+          y2 = h / 2;
+        }
+        if (center.match(/(left)/)) {
+          x = 0;
+          x2 = (cw - w) / 2;
+        } else if (center.match(/(middle)/)) {
+          x = this.canvas.width;
+          x2 = this.canvas.height - (cw - w) / 2;
+        } else if (center.match(/(center)/)) {
+          x = this.canvas.width / 2;
+          x2 = w / 2;
+        }
+      }
+      this.context.translate(x, y);
+      this.context.rotate(angle * Math.PI / 180);
+      this.context.drawImage(this.image, -x2, -y2, w, h);
+      return this;
     }, this);
     return this;
   };
